99524c833657d585f62a0918ccc80a41d653e6af
lorenzo <lorenzo.cappetti@stud.unifi.it>
2022-02-11 10:52:55 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SFML_Indiano.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML_Indiano.dir/main.cpp.o"
+  "SFML_Indiano"
+  "SFML_Indiano.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML_Indiano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
